db5deaa88aa9c16a5fb476891bc4c1e7c32bdaf8
MatheusFCE <rocha.matheus08@aluno.ifce.edu.br>
2025-02-01 19:51:47 -0300

    atualizado em 01.02.2025

--- Cadastro_de_Competencia/main.py
+++ Cadastro_de_Competencia/main.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, flash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'matheusfce'
+app.config['SECRET KEY'] = 'matheusfce'
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/cadprof', methods=['POST'])
-def cadastrar_professores():
-    return redirect(url_for('home'))
+@app.route('/cadprof', methods=['GET'])
+def cadprof():
+    return render_template('cadprof.html')
 
-@app.route('/caddisc', methods=['POST'])
-def cadastrar_disciplinas():
-    return redirect(url_for('home')) 
+@app.route('/caddisc', methods=['GET'])
+def caddisc():
+    return render_template('caddisc.html')
 
-@app.route('/vincprofdisc', methods=['POST'])
-def vincular_professores_disciplinas():
-    return redirect(url_for('home')) 
+@app.route('/vincprofdisc', methods=['GET'])
+def vincprofdisc():
+    return render_template('vincprofdisc.html')
 
-@app.route('/turmas', methods=['POST'])
+@app.route('/turmas', methods=['GET'])
 def turmas():
-    return redirect(url_for('home')) 
+    return render_template('turmas.html')
 
-if __name__ == "__main__":
+if __name__ in "__main__":
     app.run(debug=True)
