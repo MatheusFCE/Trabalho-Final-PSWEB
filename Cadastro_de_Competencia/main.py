55a4f71f3d6733c975f1c98301e79d5a2ac87c87
MatheusFCE <rocha.matheus08@aluno.ifce.edu.br>
2025-02-01 19:27:28 -0300

    atualizado em 01.02.2025

--- Cadastro_de_Competencia/main.py
+++ Cadastro_de_Competencia/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'matheusfce'
@@ -7,11 +7,21 @@ app.config['SECRET_KEY'] = 'matheusfce'
 def home():
     return render_template('home.html')
 
-@app.route('/Cadprof')
+@app.route('/cadprof', methods=['POST'])
+def cadastrar_professores():
+    return redirect(url_for('home'))
 
-def login():
-    return render_template()
+@app.route('/caddisc', methods=['POST'])
+def cadastrar_disciplinas():
+    return redirect(url_for('home')) 
 
+@app.route('/vincprofdisc', methods=['POST'])
+def vincular_professores_disciplinas():
+    return redirect(url_for('home')) 
 
-if __name__ in "__main__":
+@app.route('/turmas', methods=['POST'])
+def turmas():
+    return redirect(url_for('home')) 
+
+if __name__ == "__main__":
     app.run(debug=True)
