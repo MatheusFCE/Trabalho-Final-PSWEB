7ea05779652ae5951682f980e6ada2e1c1daebb0
MatheusFCE <rocha.matheus08@aluno.ifce.edu.br>
2025-02-08 01:44:40 -0300

    Atualizado em 08.02.2025

--- Banco de dados projeto final (PSWEB).sql
+++ Banco de dados projeto final (PSWEB).sql
@@ -3,31 +3,31 @@ CREATE DATABASE ProfEduDB;
 USE ProfEduDB;
 CREATE TABLE Professores(
 	id_professor INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	matricula INT NOT NULL,
 	nome VARCHAR(200) NOT NULL,
 	email VARCHAR(100) NOT NULL,
   	telefone VARCHAR(50) NOT NULL
 ); 
-CREATE TABLE Competencias(
-	id_competencia int primary key auto_increment, 
+CREATE TABLE Habilitacoes(
+	id_habilitacao INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
 	descricao VARCHAR(100) NOT NULL
 );
-CREATE TABLE Professores_X_Competencias(
+CREATE TABLE Professores_X_Habilitacoes(
 	id_professor INT NOT NULL,
-  	id_competencia INT NOT NULL
+  	id_habilitacao INT NOT NULL,
+    PRIMARY KEY (id_professor, id_habilitacao)
 );
 CREATE TABLE Disciplinas(
-	id_disciplina INT auto_increment PRIMARY KEY,
+	id_disciplina INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   	codigo VARCHAR(20) NOT NULL,
     	nome VARCHAR(50) NOT NULL,
     	CH CHAR(3) NOT NULL,
-    	competencia_necessaria INT NOT NULL
+    	habilitacao_necessaria INT NOT NULL
 );
 CREATE TABLE Turmas(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	id_professor INT NOT NULL,
     	id_disciplina INT NOT NULL,
-    	ano CHAR(4) NOT NULL, /*2024; 2025*/
+    	ano YEAR NOT NULL, /*2024; 2025*/
     	periodo ENUM('1' , '2') NOT NULL,
     	horario ENUM('Manhã', 'Tarde', 'Noite') NOT NULL
 );
@@ -39,57 +39,38 @@ ALTER TABLE Turmas
 ADD CONSTRAINT fk_DisciplinasXTurmas
 FOREIGN KEY (id_disciplina) REFERENCES Disciplinas(id_disciplina);
 
-ALTER TABLE Professores_X_Competencias
-ADD CONSTRAINT fk_Professores_X_Competencias_Professores
+ALTER TABLE Professores_X_Habilitacoes
+ADD CONSTRAINT fk_Professores_X_Habilitacoes_Professores
 FOREIGN KEY (id_professor) references Professores(id_professor);
 
-ALTER TABLE Professores_X_Competencias
-ADD CONSTRAINT fk_Professores_X_Competencia_Competencias
-FOREIGN KEY (id_competencia) REFERENCES Competencias(id_competencia);
+ALTER TABLE Professores_X_Habilitacoes
+ADD CONSTRAINT fk_Professores_X_Habilitacao_Habilitacoes
+FOREIGN KEY (id_habilitacao) REFERENCES Habilitacoes(id_habilitacao);
 
 ALTER TABLE Disciplinas
-ADD CONSTRAINT fk_CompetenciasXDisciplinas
-FOREIGN KEY (competencia_necessaria) REFERENCES Competencias(id_competencia);
+ADD CONSTRAINT fk_HabilitacoesXDisciplinas
+FOREIGN KEY (habilitacao_necessaria) REFERENCES Habilitacoes(id_habilitacao);
 
 DELIMITER //
 
-CREATE TRIGGER validar_competencia
+CREATE TRIGGER validar_habilitacao
 BEFORE INSERT ON Turmas
 FOR EACH ROW
 BEGIN
-    DECLARE competencia_professor INT;
+    DECLARE habilitacao_professor INT;
     
     SELECT COUNT(*)
-    INTO competencia_professor
-    FROM Professores_X_Competencias
+    INTO habilitacao_professor
+    FROM Professores_X_Habilitacoes
     WHERE id_professor = NEW.id_professor
-    AND id_competencia = (SELECT competencia_necessaria FROM Disciplinas WHERE id_disciplina = NEW.id_disciplina);
+    AND id_habilitacao = (SELECT habilitacao_necessaria FROM Disciplinas WHERE id_disciplina = NEW.id_disciplina);
 
-    IF competencia_professor = 0 THEN
+    IF habilitacao_professor = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'O professor não possui a competência necessária para lecionar esta disciplina';
+        SET MESSAGE_TEXT = 'O professor não possui a habilitação necessária para lecionar esta disciplina';
     END IF;
 END;
 
 //
 
 DELIMITER ;
-
-INSERT INTO Professores(matricula, nome, email, telefone)
-VALUES  ( 1050808, 'ANTONIO DE BARROS SERRA', 'anserra@ifce.edu.br', '(85) 9234-5678'),
-	( 508080, 'ANTONIO WENDELL DE OLIVEIRA RODRIGUES', 'wendell@ifce.edu.br', '(85) 9791-2345'),
-        ( 907727, 'FABIO ALENCAR MENDONÇA', 'fabio@ifce.edu.br', '(85) 2800-4177');
-        
-INSERT INTO Competencias(descricao) 
-VALUES  ('PROJETO DE SISTEMAS WEB'),
-	('SISTEMAS DE RÁDIO ENLACE');       
-        
-INSERT INTO Professores_X_Competencias(id_professor, id_competencia)
-VALUES 	(1,1),
-	(3,2);
-        
-INSERT INTO Disciplinas(codigo, nome, CH, competencia_necessaria)
-VALUES  ('SRE', 'SISTEMAS DE RÁDIO ENLACE', '80', 2);
-
-INSERT INTO Turmas(id_professor, id_disciplina, ano, periodo, horario)
-VALUES 	(3, 1, '2025', 2, 'Manhã');
--- Cadastro_de_Competencia/main.py
+++ Cadastro_de_Competencia/main.py
@@ -1,27 +1,50 @@
 from flask import Flask, render_template, redirect, request, flash
+import mysql.connector
+
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'matheusfce'
+app.config['SECRET_KEY'] = 'matheusfce'
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/cadprof', methods=['GET'])
+@app.route('/professores', methods=['GET'])
 def cadprof():
-    return render_template('cadprof.html')
+    return render_template('professores.html')
 
-@app.route('/caddisc', methods=['GET'])
+@app.route('/disciplinas', methods=['GET'])
 def caddisc():
-    return render_template('caddisc.html')
+    return render_template('disciplinas.html')
 
-@app.route('/vincprofdisc', methods=['GET'])
+@app.route('/vprofessoresdisciplinas', methods=['GET'])
 def vincprofdisc():
-    return render_template('vincprofdisc.html')
+    return render_template('vprofessoresdisciplinas.html')
 
 @app.route('/turmas', methods=['GET'])
 def turmas():
     return render_template('turmas.html')
 
-if __name__ in "__main__":
+
+@app.route('/cadastrarprofessor', methods=['POST'])
+def cadastrarprofessor():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    telefone = request.form.get('telefone')
+    connect_db = mysql.connector.connect(host='localhost', database='ProfEduDB', user='root', password='senha')
+
+    if connect_db.is_connected():
+        cursor = connect_db.cursor()
+        cursor.execute("INSERT INTO Professores (nome, email, telefone) VALUES (%s, %s, %s);", (nome, email, telefone))
+    if connect_db.is_connected():
+        cursor.close()
+        connect_db.close()
+
+
+    flash(f'{nome} cadastrado !!')
+
+    return redirect('/')
+
+if __name__ == "__main__":
     app.run(debug=True)
