cfa96506de71d56d318d305f880f6354680e11c9
MatheusFCE <111186662+MatheusFCE@users.noreply.github.com>
2025-02-07 21:04:34 -0300

    Update Banco de dados projeto final (PSWEB).sql

--- Banco de dados projeto final (PSWEB).sql
+++ Banco de dados projeto final (PSWEB).sql
@@ -7,20 +7,20 @@ CREATE TABLE Professores(
 	email VARCHAR(100) NOT NULL,
   	telefone VARCHAR(50) NOT NULL
 ); 
-CREATE TABLE Competencias(
-	id_competencia int primary key auto_increment, 
+CREATE TABLE Habilitacoes(
+	id_habilitacao int primary key auto_increment, 
 	descricao VARCHAR(100) NOT NULL
 );
 CREATE TABLE Professores_X_Competencias(
 	id_professor INT NOT NULL,
-  	id_competencia INT NOT NULL
+  	id_habilitacao INT NOT NULL
 );
 CREATE TABLE Disciplinas(
 	id_disciplina INT auto_increment PRIMARY KEY,
   	codigo VARCHAR(20) NOT NULL,
     	nome VARCHAR(50) NOT NULL,
     	CH CHAR(3) NOT NULL,
-    	competencia_necessaria INT NOT NULL
+    	habilitacao_necessaria INT NOT NULL
 );
 CREATE TABLE Turmas(
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -42,31 +42,31 @@ ALTER TABLE Professores_X_Competencias
 ADD CONSTRAINT fk_Professores_X_Competencias_Professores
 FOREIGN KEY (id_professor) references Professores(id_professor);
 
-ALTER TABLE Professores_X_Competencias
-ADD CONSTRAINT fk_Professores_X_Competencia_Competencias
-FOREIGN KEY (id_competencia) REFERENCES Competencias(id_competencia);
+ALTER TABLE Professores_X_Habilitacoes
+ADD CONSTRAINT fk_Professores_X_Habilitacao_Habilitacoes
+FOREIGN KEY (id_habilitacao) REFERENCES Habilitacoes(id_habilitacao);
 
 ALTER TABLE Disciplinas
-ADD CONSTRAINT fk_CompetenciasXDisciplinas
-FOREIGN KEY (competencia_necessaria) REFERENCES Competencias(id_competencia);
+ADD CONSTRAINT fk_HabilitacoesXDisciplinas
+FOREIGN KEY (habilitacao_necessaria) REFERENCES Habilitacoes(id_habilitacao);
 
 DELIMITER //
 
-CREATE TRIGGER validar_competencia
+CREATE TRIGGER validar_habilitacao
 BEFORE INSERT ON Turmas
 FOR EACH ROW
 BEGIN
-    DECLARE competencia_professor INT;
+    DECLARE habilitacao_professor INT;
     
     SELECT COUNT(*)
-    INTO competencia_professor
-    FROM Professores_X_Competencias
+    INTO habilitacao_professor
+    FROM Professores_X_Habilitacoes
     WHERE id_professor = NEW.id_professor
-    AND id_competencia = (SELECT competencia_necessaria FROM Disciplinas WHERE id_disciplina = NEW.id_disciplina);
+    AND id_habilitacao = (SELECT habilitacao_necessaria FROM Disciplinas WHERE id_disciplina = NEW.id_disciplina);
 
-    IF competencia_professor = 0 THEN
+    IF habilitacao_professor = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'O professor não possui a competência necessária para lecionar esta disciplina';
+        SET MESSAGE_TEXT = 'O professor não possui a habilitação necessária para lecionar esta disciplina';
     END IF;
 END;
 
