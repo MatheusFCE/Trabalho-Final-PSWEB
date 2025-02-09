9ded63d66736a96ffa284ad669651eb3300d24b4
MatheusFCE <rocha.matheus08@aluno.ifce.edu.br>
2025-02-09 19:26:48 -0300

    Atualizado em 09.02.2025

--- Cadastro_de_Competencia/main.py
+++ Cadastro_de_Competencia/main.py
@@ -20,24 +20,48 @@ def home():
 
 @app.route('/professores', methods=['GET'])
 def professores():
-    return render_template('professores.html')
+    try:
+        connect_db = get_db_connection()
+        cursor = connect_db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM Habilitacoes")
+        habilitacoes = cursor.fetchall()
+    except mysql.connector.Error as err:
+        flash(f"Erro ao buscar habilitações: {err}", 'danger')
+        habilitacoes = []
+    finally:
+        if cursor:
+            cursor.close()
+        if connect_db.is_connected():
+            connect_db.close()
 
+    return render_template('professores.html', habilitacoes=habilitacoes)
 
 @app.route('/cadastrarprofessor', methods=['POST'])
 def cadastrarprofessor():
     nome = request.form.get('nome')
     email = request.form.get('email')
     telefone = request.form.get('telefone')
+    id_habilitacao = request.form.get('habilitacao')
 
     try:
         connect_db = get_db_connection()
-
         cursor = connect_db.cursor()
         cursor.execute("INSERT INTO Professores (nome, email, telefone) VALUES (%s, %s, %s);", (nome, email, telefone))
         connect_db.commit()
+        id_professor = cursor.lastrowid
+
+        if id_habilitacao and id_habilitacao.strip():
+            try:
+                cursor.execute("INSERT INTO Professores_X_Habilitacoes (id_professor, id_habilitacao) VALUES (%s, %s);",
+                (id_professor, id_habilitacao))
+                connect_db.commit()
+            except mysql.connector.Error as err:
+                flash(f"Erro ao associar habilitação ao professor: {err}", 'danger')
+
         flash(f'Professor {nome} cadastrado com sucesso!', 'success')
     except mysql.connector.Error as err:
         flash(f"Erro ao cadastrar professor: {err}", 'danger')
+    
     finally:
         if cursor:
             cursor.close()
